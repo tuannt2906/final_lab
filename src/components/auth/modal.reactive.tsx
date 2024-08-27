a6b069ddc479769cdbb1f3449f1a9631178c9230
tuannt2906 <nguyentuan123.yeah@gmail.com>
2024-08-27 09:52:09 +0700

    Finish resend email.

--- src/components/auth/modal.reactive.tsx
+++ src/components/auth/modal.reactive.tsx
@@ -1,16 +1,17 @@
 'use client'
 
 import { useHasMounted } from "@/utils/customHook";
-import { Button, Form, Input, Modal, Steps } from "antd";
+import { Button, Form, Input, Modal, notification, Steps } from "antd";
 import { SmileOutlined, SolutionOutlined, UserOutlined } from '@ant-design/icons';
 import { useEffect, useState } from "react";
+import { sendRequest } from "@/utils/api";
 
 const ModalReactive = (props: any) => {
   const {isModalOpen, setIsModalOpen, userEmail} = props;
   const [current, setCurrent] = useState(0);
   const hasMounted = useHasMounted();
   const [form] = Form.useForm();
-
+  const [userId, setUserId] = useState("");
   useEffect(()=>{
     if(userEmail) {
       form.setFieldValue("email", userEmail)
@@ -18,6 +19,47 @@ const ModalReactive = (props: any) => {
   }, [userEmail]);
 
   if (!hasMounted) return <></>
+  const onFinishStep0 = async (values: any) => {
+    const {email} = values;
+    const res = await sendRequest<IBackendRes<any>>({
+      url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/retry-active`,
+      method: "POST",
+      body: {
+          email
+      }
+  })
+
+  if (res?.data) {
+    setUserId(res?.data?._id)
+    setCurrent(1);
+  } else {
+    notification.error({
+      message: "Call APIs error",
+      description: res?.message
+  })
+  };
+  
+  }
+  const onFinishStep1 = async (values: any) => {
+    const {code} = values;
+    const res = await sendRequest<IBackendRes<any>>({
+      url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/check-code`,
+      method: "POST",
+      body: {
+          code, _id: userId
+      }
+  })
+
+  if (res?.data) {
+    setCurrent(2);
+  } else {
+    notification.error({
+      message: "Call APIs error",
+      description: res?.message
+  })
+  };
+  
+  }
   return(
     <>
       <Modal 
@@ -43,39 +85,73 @@ const ModalReactive = (props: any) => {
             },
             {
               title: 'Done',
-              status: 'wait',
+              //status: 'wait',
               icon: <SmileOutlined />,
             },
           ]}
         />
         {current === 0 && 
-        <>
-          <div style={{marginTop: 20}}>Your account is not active!</div>
-          <Form
-            name="verify"
-            //onFinish={onFinish}
-            autoComplete="off"
-            layout='vertical'
-            form={form}
+          <>
+            <div style={{marginTop: 20}}>Your account is not active!</div>
+            <Form
+              name="verify"
+              onFinish={onFinishStep0}
+              autoComplete="off"
+              layout='vertical'
+              form={form}
 
-          >
-          <Form.Item
-            label=""
-            name="email"
-          >
-              <Input disabled value={userEmail}/>
-          </Form.Item>
-
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                  Resend
-              </Button>
+            >
+            <Form.Item
+              label=""
+              name="email"
+            >
+                <Input disabled value={userEmail}/>
             </Form.Item>
-          </Form>
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                    Resend
+                </Button>
+              </Form.Item>
+            </Form>
           </>
         }
         {current === 1 && 
-          <div style={{marginTop: 20}}>step 1</div>
+          <>
+            <div style={{marginTop: 20}}>Fill code in the blank!</div>
+            <Form
+              name="verify2"
+              onFinish={onFinishStep1}
+              autoComplete="off"
+              layout='vertical'
+              form={form}
+
+            >
+            <Form.Item
+              label="Code"
+              name="code"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your code!'
+                }
+              ]}
+            >
+                <Input/>
+            </Form.Item>
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                    Active
+                </Button>
+              </Form.Item>
+            </Form>
+          </>
+        }
+        {current === 2 &&
+          <div style={{marginTop: 20}}>
+            <p>Your account has been successfully activated. Please log in again!</p>
+          </div>
         }
       </Modal>
     </>
