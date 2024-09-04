24f8edfeb7cc76d8bc8bb1db4856d8066424302c
tuannt2906 <nguyentuan123.yeah@gmail.com>
2024-09-04 10:07:43 +0700

    update button homepage

--- src/components/layout/homepage.tsx
+++ src/components/layout/homepage.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { CrownOutlined } from "@ant-design/icons"
-import { Result } from "antd"
+import { Result, Button } from "antd"
+import Link from 'next/link'
 
 const HomePage = () => {
     return (
@@ -9,10 +10,23 @@ const HomePage = () => {
             <Result
                 icon={<CrownOutlined />}
                 title="Fullstack Next/Nest - createdBy @Tuan Nguyen"
+                extra={
+                    <>
+                        <Link href="/auth/login" passHref>
+                            <Button type="primary" style={{ marginRight: 10, width: 100 }}>
+                                Login
+                            </Button>
+                        </Link>
+                        <Link href="/auth/register" passHref>
+                            <Button type="primary" style={{ width: 100 }}>
+                                Register
+                            </Button>
+                        </Link>
+                    </>
+                }
             />
         </div>
     )
 }
 
 export default HomePage;
-
